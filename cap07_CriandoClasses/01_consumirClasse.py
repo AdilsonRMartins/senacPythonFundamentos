b31eda689089332bc0e15b0865bc5a6299288f8f
Adilson Rodrigues Martins <contato.adilsonmartins@gmail.com>
2023-01-31 15:34:14 -0300

    deleted underline from descricao on line 6

--- cap07_CriandoClasses/01_consumirClasse.py
+++ cap07_CriandoClasses/01_consumirClasse.py
@@ -3,5 +3,5 @@ from cap07_CriandoClasses import recibo
 dados = recibo("Adilson Martins")
 dados.valor = 51.13
 dados.descricao("Uma dúzia de pães")
-print(dados._descricao)
+print(dados.descricao)
 print(dados)
